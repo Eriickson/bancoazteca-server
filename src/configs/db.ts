85deae0e0917d0050a1756c9dd6fe550976d5a40
Erickson Manuel Holguín <erickson01d@gmail.com>
2021-10-08 18:44:00 -0400

    Adding script start

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 mongoose.Promise = global.Promise;
 
 export function startMongoose(uri: string): void {
-  // mongodb+srv://automarket_testing:Erickson0311@cluster0.hcjvp.mongodb.net/automarket-rd-v2?retryWrites=true&w=majority
   mongoose
     .connect(uri, {
       useNewUrlParser: true,
--- src/configs/envs.ts
+++ src/configs/envs.ts
@@ -1,5 +1,4 @@
 export const envs = {
   PORT: Number(process.env.PORT || 9000),
   URI_MONGODB: String(process.env.URI_MONGODB),
-  SECRECT_TOKEN_LOGIN: String(process.env.SECRECT_TOKEN_LOGIN),
 };
