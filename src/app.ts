47291fd394eff8e47933fb05bccceb1cb4cb453a
Erickson Manuel Holguín <erickson01d@gmail.com>
2021-10-09 06:45:34 -0400

    Moving db

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { startMongoose } from './configs';
+import { startMongoose } from './db';
 import { DeadlineRoutes, ProductRoutes } from './routes';
 
 export class App {
