959702bbf3a51f2c9a6cc96d9365742001ba4dee
Erickson Manuel Holguín <erickson01d@gmail.com>
2021-10-09 09:29:48 -0400

    Handler errors

--- src/controllers/product.controllers.ts
+++ src/controllers/product.controllers.ts
@@ -35,18 +35,45 @@ export class ProductController {
 
       res.json(productCreated);
     } catch (err) {
-      const message = (err as Error).message;
-      throw new Error(message);
+      let message = (err as Error).message;
+      let code = 403;
+      /* eslint-disable-next-line */
+      if ((err as any).code === 11000) {
+        /* eslint-disable-next-line */
+        const error = err as any;
+        message = `El valor ${
+          Object.values(error.keyValue)[0]
+        } ya está siendo utilizado por otro producto`;
+        code = 409;
+      }
+
+      res.status(code).json({ message });
     }
   }
   async updateProduct(req: Request, res: Response): Promise<void> {
-    const { id } = req.params;
+    try {
+      const { id } = req.params;
 
-    const product: Product = req.body;
+      const product: Product = req.body;
 
-    const productUpdated = await repository.updateProduct(id, product);
+      const productUpdated = await repository.updateProduct(id, product);
 
-    res.json(productUpdated);
+      res.json(productUpdated);
+    } catch (err) {
+      let message = (err as Error).message;
+      let code = 403;
+      /* eslint-disable-next-line */
+      if ((err as any).code === 'E11000') {
+        /* eslint-disable-next-line */
+        const error = err as any;
+        message = `El valor ${
+          Object.values(error.keyValue)[0]
+        } ya está siendo utilizado por otro producto`;
+        code = 409;
+      }
+
+      res.status(code).json({ message });
+    }
   }
   async deleteProduct(req: Request, res: Response): Promise<void> {
     const { id } = req.params;
